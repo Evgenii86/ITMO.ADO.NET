407673182e5a2f1227e2d91d50eca06145bbc4a7
Evgenii86 <73522392+Evgenii86@users.noreply.github.com>
2021-02-16 20:50:47 +0300

    Add files via upload

---
+++ LabsADO.NET/ADO.NETLab1/ITMO.LabsADO.DBConnection/ITMO.LabsADO.DBConnection/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace ITMO.LabsADO.DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listOfConnectionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionToolStripMenuItem,
+            this.disconnectionToolStripMenuItem,
+            this.listOfConnectionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 52);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectionToolStripMenuItem
+            // 
+            this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
+            this.connectionToolStripMenuItem.Size = new System.Drawing.Size(195, 48);
+            this.connectionToolStripMenuItem.Text = "Connection";
+            this.connectionToolStripMenuItem.Click += new System.EventHandler(this.connectionToolStripMenuItem_Click);
+            // 
+            // disconnectionToolStripMenuItem
+            // 
+            this.disconnectionToolStripMenuItem.Name = "disconnectionToolStripMenuItem";
+            this.disconnectionToolStripMenuItem.Size = new System.Drawing.Size(228, 48);
+            this.disconnectionToolStripMenuItem.Text = "disconnection";
+            this.disconnectionToolStripMenuItem.Click += new System.EventHandler(this.disconnectionToolStripMenuItem_Click);
+            // 
+            // listOfConnectionsToolStripMenuItem
+            // 
+            this.listOfConnectionsToolStripMenuItem.Name = "listOfConnectionsToolStripMenuItem";
+            this.listOfConnectionsToolStripMenuItem.Size = new System.Drawing.Size(290, 48);
+            this.listOfConnectionsToolStripMenuItem.Text = "List of connections";
+            this.listOfConnectionsToolStripMenuItem.Click += new System.EventHandler(this.listOfConnectionsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listOfConnectionsToolStripMenuItem;
+    }
+}
+
---
+++ LabsADO.NET/ADO.NETLab2/ITMO.LabsADO.DBConnection/ITMO.LabsADO.DBConnection/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace ITMO.LabsADO.DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listOfConnectionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionToolStripMenuItem,
+            this.disconnectionToolStripMenuItem,
+            this.listOfConnectionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(997, 60);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectionToolStripMenuItem
+            // 
+            this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
+            this.connectionToolStripMenuItem.Size = new System.Drawing.Size(195, 45);
+            this.connectionToolStripMenuItem.Text = "Connection";
+            this.connectionToolStripMenuItem.Click += new System.EventHandler(this.connectionToolStripMenuItem_Click);
+            // 
+            // disconnectionToolStripMenuItem
+            // 
+            this.disconnectionToolStripMenuItem.Name = "disconnectionToolStripMenuItem";
+            this.disconnectionToolStripMenuItem.Size = new System.Drawing.Size(228, 45);
+            this.disconnectionToolStripMenuItem.Text = "disconnection";
+            this.disconnectionToolStripMenuItem.Click += new System.EventHandler(this.disconnectionToolStripMenuItem_Click);
+            // 
+            // listOfConnectionsToolStripMenuItem
+            // 
+            this.listOfConnectionsToolStripMenuItem.Name = "listOfConnectionsToolStripMenuItem";
+            this.listOfConnectionsToolStripMenuItem.Size = new System.Drawing.Size(290, 45);
+            this.listOfConnectionsToolStripMenuItem.Text = "List of connections";
+            this.listOfConnectionsToolStripMenuItem.Click += new System.EventHandler(this.listOfConnectionsToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Список продуктов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(418, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(337, 259);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 268;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Транзакция";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(280, 68);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Сколько продуктов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(412, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Сколько продуктов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 565);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listOfConnectionsToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
