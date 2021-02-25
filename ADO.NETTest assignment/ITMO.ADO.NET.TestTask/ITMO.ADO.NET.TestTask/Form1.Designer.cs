a2e49c8ff7ca6e240ff0cd93613655b3fd9bc1d5
Evgenii86 <73522392+Evgenii86@users.noreply.github.com>
2021-02-25 09:28:48 +0300

    Add files via upload

---
+++ ADO.NETTest assignment/ITMO.ADO.NET.TestTask/ITMO.ADO.NET.TestTask/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace ITMO.ADO.NET.TestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MyDBdataGrid = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MyDBdataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MyDBdataGrid
+            // 
+            this.MyDBdataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MyDBdataGrid.Location = new System.Drawing.Point(13, 29);
+            this.MyDBdataGrid.MultiSelect = false;
+            this.MyDBdataGrid.Name = "MyDBdataGrid";
+            this.MyDBdataGrid.RowHeadersWidth = 102;
+            this.MyDBdataGrid.RowTemplate.Height = 40;
+            this.MyDBdataGrid.Size = new System.Drawing.Size(2405, 1397);
+            this.MyDBdataGrid.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 1461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(538, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сохранить изменения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1605, 1461);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(487, 67);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Переключить таблицу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2621, 1571);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MyDBdataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MyDBdataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView MyDBdataGrid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ADO.NETTest assignment/ITMO.ADO.NET.TestTask/ITMO.ADO.NET.TestTask/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ITMO.ADO.NET.TestTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection MyDBConnection = new SqlConnection("Integrated Security=SSPI; Persist Security Info=False; Initial Catalog=MyDB;Data Source=MOJ-PK\\SQLEXPRESS");
+        private SqlDataAdapter MyDataAdapter;
+        private DataSet MyDataset = new DataSet("MyDB");
+        private DataTable CustomersTable = new DataTable("Customers");
+        private DataTable OrdersTable = new DataTable("Orders");
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (MyDataAdapter = new SqlDataAdapter("SELECT * FROM Customers", MyDBConnection))
+            {
+                MyDataset.Tables.Add(CustomersTable);
+                MyDataAdapter.Fill(MyDataset.Tables["Customers"]);
+                MyDBdataGrid.DataSource = MyDataset.Tables["Customers"];
+                MyDataAdapter.SelectCommand.CommandText = "SELECT * FROM Orders";
+                MyDataset.Tables.Add(OrdersTable);
+                MyDataAdapter.Fill(MyDataset.Tables["Orders"]);
+
+            }
+
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlCommandBuilder cmd1 = new SqlCommandBuilder(MyDataAdapter))
+            {
+                MyDataset.EndInit();
+                SqlCommand command1 = new SqlCommand("SELECT * FROM Customers", MyDBConnection);
+                MyDataAdapter.SelectCommand = command1;
+                MyDataAdapter.Update(MyDataset.Tables["Customers"]);
+                SqlCommand command2 = new SqlCommand("SELECT * FROM Orders", MyDBConnection);
+                MyDataAdapter.SelectCommand = command2;
+                MyDataAdapter.Update(MyDataset.Tables["Orders"]);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MyDBdataGrid.DataSource == MyDataset.Tables["Orders"])
+                MyDBdataGrid.DataSource = MyDataset.Tables["Customers"];
+            else
+                MyDBdataGrid.DataSource = MyDataset.Tables["Orders"];
+        }
+    }
+
+
+}
